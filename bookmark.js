a9258de7050e5e739bd2bc72628099f73fcf560d
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2018-10-14 14:55:44 +0100

    Simplified code

--- bookmark.js
+++ bookmark.js
@@ -1,9 +1,1 @@
-javascript: (function() {
-    var w = window.open("about:blank");
-    var d = w.document;
-    d.write("<!DOCTYPE html><html><head></head><body></body></html>"), d.close();
-    var f = d.createElement("form");
-    f.setAttribute("method", "post"), f.setAttribute("action", "http://localhost/?url=" + encodeURIComponent(location.href));
-    var i = d.createElement("input");
-    i.setAttribute("type", "hidden"), i.setAttribute("name", "DOM"), i.setAttribute("value", encodeURIComponent(document.documentElement.innerHTML)), d.body.appendChild(f).appendChild(i), f.submit();
-})()
+javascript: window.open("http://localhost/?url=" + encodeURIComponent(location.href));
